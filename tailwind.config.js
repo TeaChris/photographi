c76b0da7306b20c6fbb69f24098f90b6cb249e83
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-02-26 18:47:31 +0100

    styles(index.css): applying general styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff7b02',
+        primaryVariant: '#e5a55d',
+        bg1: '#140021',
+        bg2: '#1e0032',
+        bg3: '#25003e',
+        bg4: '#36005a',
+        light: '#d0b8e0',
+        white: 'hsl(0, 0%, 90%)',
+      },
+    },
   },
   plugins: [],
 }
