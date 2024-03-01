2993128d5d36eb9eb28d288669da49d8b49577e5
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-03-01 13:21:20 +0100

    Update header layout and add background image (generated by blackbox.ai )

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { Navbar } from './components/navbar'
 
 function App() {
   return (
-    <main className="space-y-16">
-      <header className="space-y-12">
-        <Navbar />
+    <div className="w-full space-y-[4.5rem]">
+      <Navbar />
+
+      <main className="space-y-16 pt-1">
         <Header />
-      </header>
-    </main>
+      </main>
+    </div>
   )
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,12 @@
 export function Header() {
-  return <section className="mt-12">Header!</section>
+  return (
+    <section className="w-full h-[calc(100vh-5rem)] relative top-0 overflow-hidden bg-gradient from-bg3 to-bg1">
+      <header className="w-lg mx-auto grid grid-cols-[43% 53%] gap-[4%] mt-40 relative">
+        <div className="header_img-bg"></div>
+        <div>
+          <img src="/" />
+        </div>
+      </header>
+    </section>
+  )
 }
